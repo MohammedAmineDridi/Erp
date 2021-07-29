c7f40a3b756dd6314470f5d10efe12ec92faf5df
MohammedAmineDridi <mohammedamine.dridi.esprit@gmail.com>
2021-07-29 19:11:44 +0100

    first commit

---
+++ src/swing/macros/Macro.java
@@ -0,0 +1,294 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package swing.macros;
+
+import java.io.IOException;
+
+/**
+ *
+ * @author dridi family
+ */
+public class Macro {
+    
+    
+    public Macro(){}
+    
+    
+    
+    
+    public static void macro_article() throws IOException{
+    
+            
+        // macro mail
+        
+        
+             Process run;
+
+         String commande_mail = "cscript D:\\creation_article.vbs";
+     
+            run = Runtime.getRuntime().exec(commande_mail);
+            
+            System.out.println(" macro article mrigl ");
+            
+            
+            
+        
+        } 
+        
+    
+    
+    
+    
+    
+    public static void macro_article_notif() throws IOException{
+    
+    
+    
+    
+             Process run;
+
+         String commande_mail = "cscript D:\\creation_article_notif.vbs";
+     
+            run = Runtime.getRuntime().exec(commande_mail);
+            
+            System.out.println(" macro article notification mrigl ");
+            
+    
+    
+    
+    }
+    
+ 
+    
+    
+    
+    
+    
+    
+    
+    public static void macro_commande1() throws IOException{
+    
+    
+    
+             Process run;
+
+         String commande_mail = "cscript D:\\creation_commande1.vbs";
+     
+            run = Runtime.getRuntime().exec(commande_mail);
+            
+            System.out.println(" macro creation commande 1  mrigl ");
+            
+    
+    
+    
+    
+    }
+    
+    
+    
+    
+    
+    
+    
+    public static void macro_commande2() throws IOException{
+    
+    
+    
+             Process run;
+
+         String commande_mail = "cscript D:\\creation_commande2.vbs";
+     
+            run = Runtime.getRuntime().exec(commande_mail);
+            
+            System.out.println(" macro creation commande 2  mrigl ");
+            
+    
+    
+    
+    
+    }
+    
+    
+    public static void macro_commande_notif() throws IOException{
+    
+    
+    
+             Process run;
+
+         String commande_mail = "cscript D:\\creation_commande_notif.vbs";
+     
+            run = Runtime.getRuntime().exec(commande_mail);
+            
+            System.out.println(" macro creation commande notif  mrigl ");
+            
+    
+    
+    
+    
+    }
+    
+    
+    
+    
+    
+    
+    public static void macro_suppresion_commande1() throws IOException{
+    
+    
+    
+             Process run;
+
+         String commande_mail = "cscript D:\\suppression_commande1.vbs";
+     
+            run = Runtime.getRuntime().exec(commande_mail);
+            
+            System.out.println(" macro suppression commande 1  mrigl ");
+            
+    
+    
+    
+    
+    }
+    
+    
+    
+    public static void macro_suppresion_commande2() throws IOException{
+    
+    
+    
+             Process run;
+
+         String commande_mail = "cscript D:\\suppression_commande2.vbs";
+     
+            run = Runtime.getRuntime().exec(commande_mail);
+            
+            System.out.println(" macro suppression commande 2  mrigl ");
+            
+    
+    
+    
+    
+    }
+    
+   
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    public static void macro_creation_pic1() throws IOException{
+    
+    
+    
+             Process run;
+
+         String commande_mail = "cscript D:\\creation_pic1.vbs";
+     
+            run = Runtime.getRuntime().exec(commande_mail);
+            
+            System.out.println(" macro creation pic 1  mrigl ");
+            
+    
+    
+    
+    
+    }
+    
+    
+    
+    
+    
+    
+    
+    public static void macro_creation_pic2() throws IOException{
+    
+    
+    
+             Process run;
+
+         String commande_mail = "cscript D:\\creation_pic2.vbs";
+     
+            run = Runtime.getRuntime().exec(commande_mail);
+            
+            System.out.println(" macro creation pic 2  mrigl ");
+            
+    
+    
+    
+    
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    public static void macro_creation_pic3() throws IOException{
+    
+    
+    
+             Process run;
+
+         String commande_mail = "cscript D:\\creation_pic3.vbs";
+     
+            run = Runtime.getRuntime().exec(commande_mail);
+            
+            System.out.println(" macro creation pic 3  mrigl ");
+            
+    
+    
+    
+    
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    public static void macro_creation_pdp() throws IOException{
+    
+    
+    
+             Process run;
+
+         String commande_mail = "cscript D:\\creation_pdp.vbs";
+     
+            run = Runtime.getRuntime().exec(commande_mail);
+            
+            System.out.println(" macro creation pdp  mrigl ");
+            
+    
+    
+    
+    
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+}
